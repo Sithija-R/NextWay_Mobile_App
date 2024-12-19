100fb5cc7c58340faebd5936ebb096b3f7d5947d
blockdeveloper25 <sujairblockdev@gmail.com>
2024-12-19 23:22:42 +0530

    added use navigate for about page

--- NextWay_Client/NextWay/app/(appScreens)/(userScreens)/(pages)/about.jsx
+++ NextWay_Client/NextWay/app/(appScreens)/(userScreens)/(pages)/about.jsx
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native';
 
 export default function About() {
+  const navigation = useNavigation(); // Access the navigation object
+  
   return (
     <View>
       <Text>About</Text>
