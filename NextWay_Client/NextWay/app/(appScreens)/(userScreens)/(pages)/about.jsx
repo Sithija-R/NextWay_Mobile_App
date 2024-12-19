be904fb1398174e21397dfbe92b7d87b5f564af5
blockdeveloper25 <sujairblockdev@gmail.com>
2024-12-19 23:26:08 +0530

    added styles

--- NextWay_Client/NextWay/app/(appScreens)/(userScreens)/(pages)/about.jsx
+++ NextWay_Client/NextWay/app/(appScreens)/(userScreens)/(pages)/about.jsx
@@ -4,10 +4,16 @@ import { useNavigation } from '@react-navigation/native';
 
 export default function About() {
   const navigation = useNavigation(); // Access the navigation object
-  
+
   return (
     <View>
-      <Text>About</Text>
+      <Text style={styles.text}>About</Text>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+});
